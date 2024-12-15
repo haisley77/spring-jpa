89e7759c67b069922df4d32e947b0b1da4878ccd
haisley77 <hahi1108@gmail.com>
2024-12-15 14:06:57 +0900

    feat: 간단한 주문 조회 V1 (엔티티를 직접 노출)

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -15,8 +15,7 @@ public class JpashopApplication {
 	@Bean
 	Hibernate5JakartaModule hibernate5Module() {
 		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
-		//강제 지연 로딩 설정
-		hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+//		hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
 		return hibernate5JakartaModule;
 	}
 
--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,6 +24,12 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order: all) {
+            order.getMember()       // 여기까지는 프록시 객체
+                    .getName();     // member 쿼리를 db에 날리면서 Lazy 강제 초기화
+            order.getDelivery()     // 여기까지는 프록시 객체
+                    .getAddress();  // delivery 쿼리를 db에 날리면서 Lazy 강제 초기화
+        }
         return all;
     }
 
